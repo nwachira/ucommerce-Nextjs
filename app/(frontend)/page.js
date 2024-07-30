0e8cf88b58ca25a4acc6c90095fcb5364ac31a7d
nwachira <nwachira@unganishanetworks.com>
2024-07-30 16:43:30 +0300

    first deployment

---
+++ app/(frontend)/page.js
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+  return (
+    <div className=" flex justify-center items-center ">
+
+     <h1 className=" text-xl text-lime-500 shadow-md flex justify-center items-center">
+     Bryan Welcome to Ucommerce 
+     </h1>
+
+    </div>
+  );
+}
